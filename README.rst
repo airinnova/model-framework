aee313fe64ecd5a0c61f5afe21968e95e7ba7acf
Aaron Dettmann <dettmann@kth.se>
2020-04-25 18:37:10 +0200

    First commit

---
+++ README.rst
@@ -0,0 +1,42 @@
+.. image:: https://img.shields.io/pypi/v/model-framework.svg?style=flat
+   :target: https://pypi.org/project/model-framework/
+   :alt: Latest PyPI version
+
+.. image:: https://readthedocs.org/projects/mframework/badge/?version=latest
+    :target: https://mframework.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+.. image:: https://img.shields.io/badge/license-Apache%202-blue.svg
+    :target: https://github.com/airinnova/model-framework/blob/master/LICENSE.txt
+    :alt: License
+
+.. image:: https://travis-ci.org/airinnova/model-framework.svg?branch=master
+    :target: https://travis-ci.org/airinnova/model-framework
+    :alt: Build status
+
+.. image:: https://codecov.io/gh/airinnova/model-framework/branch/master/graph/badge.svg
+    :target: https://codecov.io/gh/airinnova/model-framework
+    :alt: Coverage
+
+|
+
+.. image:: https://raw.githubusercontent.com/airinnova/model-framework/master/docs/source/_static/images/logo.png
+   :target: https://github.com/airinnova/model-framework/
+   :alt: logo
+
+TODO
+
+
+Installation
+============
+
+*Model-Framework* is available on `PyPI <https://pypi.org/project/model-framework/>`_ and may simply be installed with
+
+.. code::
+
+    pip install model-framework
+
+License
+=======
+
+**License:** Apache-2.0
---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import setuptools
+import os
+
+from src.mframework.__version__ import __version__
+
+NAME = 'model-framework'
+PACKAGE = 'mframework'
+VERSION = __version__
+AUTHOR = 'Aaron Dettmann'
+EMAIL = 'dettmann@kth.se'
+DESCRIPTION = ''
+URL = 'Framework to build consistent model interfaces '
+REQUIRES_PYTHON = '>=3.6.0'
+REQUIRED = []
+README = 'README.rst'
+PACKAGE_DIR = 'src/'
+LICENSE = 'Apache License 2.0'
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, README), "r") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=long_description,
+    url=URL,
+    include_package_data=True,
+    package_dir={'': PACKAGE_DIR},
+    license=LICENSE,
+    packages=[PACKAGE],
+    python_requires=REQUIRES_PYTHON,
+    install_requires=REQUIRED,
+    # See: https://pypi.org/classifiers/
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering",
+    ],
+)
